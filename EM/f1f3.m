96e8c385ef8757b9a441ab82a4efe5addd97a8ed
kejriwalgauv <gauravbittu23@gmail.com>
2024-12-27 16:55:11 +0530

    Add files via upload

---
+++ EM/f1f3.m
@@ -0,0 +1,62 @@
+X = [peak ramprate];
+k = 1:10;
+nK = numel(k);
+Sigma = {'diagonal','full'};
+nSigma = numel(Sigma);
+SharedCovariance = {true,false};
+SCtext = {'true','false'};
+nSC = numel(SharedCovariance);
+RegularizationValue = 0.01;
+options = statset('MaxIter',10000);
+% Preallocation
+gm = cell(nK,nSigma,nSC);
+%aic = zeros(nK,nSigma,nSC);
+%bic = zeros(nK,nSigma,nSC);
+converged = false(nK,nSigma,nSC);
+
+% Fit all models
+for m = 1:nSC;
+    for j = 1:nSigma;
+        for i = 1:nK;
+            gm{i,j,m} = fitgmdist(X,k(i),...
+                'CovarianceType',Sigma{j},...
+                'SharedCovariance',SharedCovariance{m},...
+                'RegularizationValue',RegularizationValue,...
+                'Options',options);
+            %aic(i,j,m) = gm{i,j,m}.AIC;
+            %bic(i,j,m) = gm{i,j,m}.BIC;
+            converged(i,j,m) = gm{i,j,m}.Converged;
+        end
+    end
+end
+
+allConverge = (sum(converged(:)) == nK*nSigma*nSC)
+
+gmBest = gm{3,2,2};
+clusterX = cluster(gmBest,X);
+kGMM = gmBest.NumComponents;
+d = 1000;
+x1 = linspace(min(X(:,1)) - 1,max(X(:,1)) + 1,d);
+x2 = linspace(min(X(:,2)) - 1,max(X(:,2)) + 1,d);
+[x1grid,x2grid] = meshgrid(x1,x2);
+X0 = [x1grid(:) x2grid(:)];
+mahalDist = mahal(gmBest,X0);
+threshold = sqrt(chi2inv(0.99,2));
+
+figure;
+h1 = gscatter(X(:,1),X(:,2),clusterX);
+hold on;
+for j = 1:kGMM;
+    idx = mahalDist(:,j)<=threshold;
+    Color = h1(j).Color*0.75 + -0.5*(h1(j).Color - 1);
+    h2 = plot(X0(idx,1),X0(idx,2),'.','Color',Color,'MarkerSize',1);
+    uistack(h2,'bottom');
+end
+h3 = plot(gmBest.mu(:,1),gmBest.mu(:,2),'kx','LineWidth',2,'MarkerSize',10);
+title('Ramp rate vs Peak power');
+xlabel('Peak power(kWhh)');
+ylabel('Ramp rate');
+legend(h1,'Cluster 1','Cluster 2','Cluster 3' );
+hold off
+
+
